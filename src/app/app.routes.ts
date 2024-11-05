74aa7a902563de8b09ddbe1e6d5a76e3a140748d
selvavaithi <selvavaithi@gmail.com>
2024-11-05 20:14:10 +0000

    feat(routing): Add login page route
    This commit integrates the login page into the application's routing system.
    
    The following changes were made:
    
    - Imported the `LoginComponent` into the `app.routes.ts` file.
    - Defined a route for the login page with the path `/login` and associated it with the `LoginComponent`.
    - Added a redirect to `/login` for the root path (`/`) to ensure users are directed to the login page on initial visit.
    
    These changes enhance the application's navigation and user experience.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:'login', component:LoginComponent},
+{path:'signup', component:SignUpComponent},
+{path:'home', component:HomeComponent},
+{path:'contact', component:ContactComponent},
+{path:'', redirectTo:'/home', pathMatch:'full'}
+
+];
